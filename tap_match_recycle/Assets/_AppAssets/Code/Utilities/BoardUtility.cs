56f4c71b8ad4a6f1b8d68b75f3bc8b76c43e45d8
Dani Garcia Fernandez <danigfe.dev@gmail.com>
2023-07-31 01:07:38 +0200

    Fix BoardUtility IsCoordinateInBounds while chekcing first column and first row

--- tap_match_recycle/Assets/_AppAssets/Code/Utilities/BoardUtility.cs
+++ tap_match_recycle/Assets/_AppAssets/Code/Utilities/BoardUtility.cs
@@ -7,7 +7,7 @@ namespace _AppAssets.Code.Utilities
             int width = board.GetLength(0);
             int height = board.GetLength(1);
             
-            return coordinates.Row > 0 && coordinates.Row < height && coordinates.Column > 0 &&
+            return coordinates.Row >= 0 && coordinates.Row < height && coordinates.Column >= 0 &&
                    coordinates.Column < width;
         }
     }
