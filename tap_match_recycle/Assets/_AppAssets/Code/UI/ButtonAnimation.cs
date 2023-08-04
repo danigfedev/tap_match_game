001cabe6e594fb072bb35f77d4da956264d97ad4
Dani Garcia Fernandez <danigfe.dev@gmail.com>
2023-08-04 17:40:18 +0200

    Button Animation component integrated in project

---
+++ tap_match_recycle/Assets/_AppAssets/Code/UI/ButtonAnimation.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace EG.EndlessShapes.UI
+{
+    [RequireComponent(typeof(Button))]
+    public class ButtonAnimation : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        private enum AnimationDirections
+        {
+            ScaleUp,
+            ScaleDown,
+        }
+
+        [SerializeField] private Transform _animationTarget;
+        [SerializeField] private float _shrinkScale = 0.85f;
+        [SerializeField] private float _animationSpeed = 2f;
+
+        private float _initialScale;
+        private Button _button;
+        private bool _isDirty;
+        private Coroutine _animationCoroutine;
+        private float _elapsedTime = 0;
+
+        private void Awake()
+        {
+            if(_animationTarget == null)
+            {
+                _animationTarget = transform;
+            }
+
+            _initialScale = _animationTarget.localScale.x; //Buttons should be scaled equally in every axis
+
+            _button = GetComponent<Button>();
+        }
+
+
+        // Reset behaviour on click
+        public void OnDisable()
+        {
+            if (!_isDirty)
+            {
+                return;
+            }
+
+            if (_animationCoroutine != null)
+            {
+                StopCoroutine(_animationCoroutine);
+                _animationCoroutine = null;
+            }
+
+            _animationTarget.localScale = _initialScale * Vector3.one;
+            
+            _isDirty = false;
+        }
+
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            if (_button.interactable)
+            {
+                PlayAnimation(AnimationDirections.ScaleDown);
+            }
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            if (_button.interactable)
+            {
+                PlayAnimation(AnimationDirections.ScaleUp);
+            }
+        }
+
+        private void PlayAnimation(AnimationDirections direction)
+        {
+            _isDirty = true;
+
+            if (_animationCoroutine != null)
+            {
+                StopCoroutine(_animationCoroutine);
+            }
+
+            _animationCoroutine = StartCoroutine(AnimateButton(direction));
+        }
+
+        private IEnumerator AnimateButton(AnimationDirections direction)
+        {
+            float start, end;
+
+            switch (direction)
+            {
+                case AnimationDirections.ScaleUp: 
+                    start = _shrinkScale;
+                    end = _initialScale;
+                    break;
+                case AnimationDirections.ScaleDown:
+                    start = _initialScale;
+                    end = _shrinkScale;
+                    break;
+                default:
+                    start = _shrinkScale;
+                    end = _initialScale;
+                    break;
+            }
+
+            _elapsedTime = 0;
+            float newScale = start;
+
+            while (_elapsedTime <= 1)
+            {
+                newScale = Mathf.Lerp(start, end, _elapsedTime);
+                _animationTarget.localScale = newScale * Vector3.one;
+
+                yield return null;
+
+                _elapsedTime += _animationSpeed * Time.deltaTime;
+            }
+
+            //The complete animation cycle has ended (scale down -> up)
+            if (direction == AnimationDirections.ScaleUp)
+            {
+                _isDirty = false;
+            }
+
+            _animationCoroutine = null;
+        }
+    }
+}
